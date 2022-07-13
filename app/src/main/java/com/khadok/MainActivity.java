4cb9df1de53f849134fcc5f523d4a9199a4dce24
Shariful Islam Rokib <rokibislam69@gmail.com>
2022-07-13 19:31:19 +0600

    bug fixes

--- app/src/main/java/com/khadok/MainActivity.java
+++ app/src/main/java/com/khadok/MainActivity.java
@@ -27,11 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<MainModel> list = new ArrayList<>();
 
-        list.add(new MainModel(R.drawable.burger, "Burger", "5", "Chicken Burger with extra petty"));
-        list.add(new MainModel(R.drawable.pizza, "Pizza", "10", "Served with chicken, peperoni, black peper, sausage, mushroom"));
-        list.add(new MainModel(R.drawable.fries, "Fries", "3", "Potato fries served with tomato sauce"));
-        list.add(new MainModel(R.drawable.chicken_biryani, "Chicken Biryani", "8", "Biryani served with chicken"));
-        list.add(new MainModel(R.drawable.seafood, "Seafood", "15", "Served with crab, prawn, salmon fish"));
+        list.add(new MainModel(R.drawable.burger, "Burger", "180", "Chicken Burger with extra petty"));
+        list.add(new MainModel(R.drawable.pizza, "Pizza", "450", "Served with chicken, peperoni, black peper, sausage, mushroom"));
+        list.add(new MainModel(R.drawable.fries, "Fries", "150", "Potato fries served with tomato sauce"));
+        list.add(new MainModel(R.drawable.chicken_biryani, "Chicken Biryani", "220", "Biryani served with chicken"));
+        list.add(new MainModel(R.drawable.seafood, "Seafood", "650", "Served with crab, prawn, salmon fish"));
 
         MainAdapter adapter = new MainAdapter(list, this);
         binding.recyclerview.setAdapter(adapter);
